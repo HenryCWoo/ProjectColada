23c5e2ae9fc9b479fd890970930b6ab5654209c0
hcwoo2 <hcwoo2@illinois.edu>
2018-04-24 00:43:04 -0500

    still need to record ingredients in update and add

--- ProjectColada2/app/src/main/java/com/example/henry/projectcolada/MainActivity/Recipe/EditRecipe.java
+++ ProjectColada2/app/src/main/java/com/example/henry/projectcolada/MainActivity/Recipe/EditRecipe.java
@@ -9,10 +9,12 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -52,8 +54,124 @@ public class EditRecipe extends AppCompatActivity {
     private String titleText, aboutText, instrText, authorName, authorID;
     private FirebaseAuth auth;
     private View overlay;
+    private LinearLayout ingredLL;
 
     private String mode, oldDrinkName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_recipe);
+
+        Intent prevIntent = getIntent();
+        mode = prevIntent.getStringExtra("mode");
+
+        editPB = (ProgressBar) findViewById(R.id.edit_pb);
+        overlay = (View) findViewById(R.id.overlay);
+
+        spiritSpin = (Spinner) findViewById(R.id.spirit_spinner);
+        String[] spirits = new String[]{"N/A", "Absinthe", "Amaretto", "Amaro", "Aquavit", "Arak", "Armagnac", "Baiju", "Beer",
+                "Bourbon/American Whiskey", "Brandy/Cognac", "Cachaca", "Calvados", "Canadian Whiskey", "Champagne & Sparkling Wine",
+                "Cider", "Cognar", " Eau-de-vie", "Gin", "Irish Whiskey", "Japanese Whiskey", "Liqueurs", "Mead", "Mezcal", "Non alcoholic", "Other Whiskey", "Pastis", "Pisco", "Rhum Agricole", "Rum", "Rye Whiskey", "Scotch",
+                "Sherry", "Shochu", "Soju", "Sotol", "Taiwanese Whiskey", "Tequila", "Vermouth/Aperitif Wine", "Vodka", "Whiskey", "Wine"};
+        ArrayAdapter<String> spiritAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, spirits);
+        spiritSpin.setAdapter(spiritAdapter);
+
+        glassSpin = (Spinner) findViewById(R.id.glass_spinner);
+        String[] glasses = new String[]{"N/A", "Beer Mug", "Champagne Flute", "Cocktail/Martini", "Cordial",
+                "Coupe/Coupette", "Highball/Collins", "Hurricane", "Margarita", "Mason Jar", "Moscow Mule", "Mug", "Old Fashioned/Rocks", "Pint", "Punch", "Shot", "Snifter", "Wine Glass"};
+        ArrayAdapter<String> glassAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, glasses);
+        glassSpin.setAdapter(glassAdapter);
+
+        typeSpin = (Spinner) findViewById(R.id.type_spinner);
+        String[] types = new String[]{"N/A", "Classics", "Frozen/Blended", "Hot", "Margaritas", "Martinis", "Modern Classics", "Non-Alcoholic", "Punches", "Shots", "Tiki/Tropical"};
+        ArrayAdapter<String> typeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, types);
+        typeSpin.setAdapter(typeAdapter);
+
+        strengthSpin = (Spinner) findViewById(R.id.strength_spinner);
+        String[] strengths = new String[]{"N/A", "Light", "Medium", "Strong"};
+        ArrayAdapter<String> strengthAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, strengths);
+        strengthSpin.setAdapter(strengthAdapter);
+
+        difficultySpin = (Spinner) findViewById(R.id.difficulty_spinner);
+        String[] difficulties = new String[]{"N/A", "Simple", "Medium", "Complicated"};
+        ArrayAdapter<String> difficultyAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, difficulties);
+        difficultySpin.setAdapter(difficultyAdapter);
+
+        themeSpin = (Spinner) findViewById(R.id.theme_spinner);
+        String[] themes = new String[]{"N/A", "Brunch", "chocolate", "coffee", "Dessert", "Fall", "Holiday Season", "Romantic", "Sports", "Spring", "Summer", "Travel", "Winter"};
+        ArrayAdapter<String> themeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, themes);
+        themeSpin.setAdapter(themeAdapter);
+
+        serveSpin = (Spinner) findViewById(R.id.served_spinner);
+        String[] serves = new String[]{"N/A", "Chipped Block Ice", "Crushed Ice", "Frozen", "Hot/Warm", "Large Ice Cube", "Neat/Up", "On the Rocks"};
+        ArrayAdapter<String> serveAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, serves);
+        serveSpin.setAdapter(serveAdapter);
+
+        prepSpin = (Spinner) findViewById(R.id.prep_spinner);
+        String[] preps = new String[]{"N/A", "Blended", "Cooked/Heated", "Poured", "Shaken", "Stirred"};
+        ArrayAdapter<String> prepAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, preps);
+        prepSpin.setAdapter(prepAdapter);
+
+        flavor_spin = (Spinner) findViewById(R.id.flavor_spinner);
+        String[] flavors = new String[]{"N/A", "Bitter", "Bubbly", "Creamy", "Fruity/Citrus-forward", "Herbaceous", "Salty/Savory", "Sour", "Spicy", "Spirit-forward", "Sweet"};
+        ArrayAdapter<String> flavorAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, flavors);
+        flavor_spin.setAdapter(flavorAdapter);
+
+        title = (EditText) findViewById(R.id.title);
+        about = (EditText) findViewById(R.id.about);
+        instructions = (EditText) findViewById(R.id.instructions);
+
+        auth = FirebaseAuth.getInstance();
+        authorID = auth.getCurrentUser().getUid();
+        switch (mode) {
+            case "add":
+                break;
+            case "edit":
+                oldDrinkName = prevIntent.getStringExtra("drinkName");
+                title.setText(prevIntent.getStringExtra("drinkName"), TextView.BufferType.EDITABLE);
+                about.setText(prevIntent.getStringExtra("about"), TextView.BufferType.EDITABLE);
+                instructions.setText(prevIntent.getStringExtra("instructions"), TextView.BufferType.EDITABLE);
+                String[] attributes = prevIntent.getStringArrayExtra("Attributes");
+                spiritSpin.setSelection(getIndex(spiritSpin, prevIntent.getStringExtra("spirit")));
+                glassSpin.setSelection(getIndex(glassSpin, attributes[9]));
+                typeSpin.setSelection(getIndex(typeSpin, attributes[10]));
+                strengthSpin.setSelection(getIndex(strengthSpin, attributes[6]));
+                difficultySpin.setSelection(getIndex(difficultySpin, attributes[7]));
+                themeSpin.setSelection(getIndex(themeSpin, attributes[8]));
+                serveSpin.setSelection(getIndex(serveSpin, attributes[11]));
+                prepSpin.setSelection(getIndex(prepSpin, attributes[5]));
+                flavor_spin.setSelection(getIndex(flavor_spin, prevIntent.getStringExtra("flavor")));
+        }
+
+        ingredLL = (LinearLayout) findViewById(R.id.ingred_LL);
+        ingredAdd = (ImageView) findViewById(R.id.add_ingredient);
+
+        ingredAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LinearLayout newLL = new LinearLayout(EditRecipe.this);
+                newLL.setOrientation(LinearLayout.HORIZONTAL);
+                newLL.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                EditText addIngredEntry = new EditText(EditRecipe.this);
+                EditText addPartEntry = new EditText(EditRecipe.this);
+
+                addIngredEntry.setTextSize(14);
+                LinearLayout.LayoutParams ingredEntryParam = new LinearLayout.LayoutParams(0,ViewGroup.LayoutParams.WRAP_CONTENT, 7f);
+                addIngredEntry.setLayoutParams(ingredEntryParam);
+                addIngredEntry.setHint("Ingredient");
+
+                addPartEntry.setTextSize(14);
+                LinearLayout.LayoutParams partEntryParam = new LinearLayout.LayoutParams(0,ViewGroup.LayoutParams.WRAP_CONTENT, 1f);
+                addPartEntry.setLayoutParams(partEntryParam);
+                addPartEntry.setHint("Part");
+
+                newLL.addView(addIngredEntry);
+                newLL.addView(addPartEntry);
+                ingredLL.addView(newLL);
+            }
+        });
+    }
 
     // handle button activities
     @Override
@@ -277,92 +395,7 @@ public class EditRecipe extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_recipe);
 
-        Intent prevIntent = getIntent();
-        mode = prevIntent.getStringExtra("mode");
-
-        editPB = (ProgressBar) findViewById(R.id.edit_pb);
-        overlay = (View) findViewById(R.id.overlay);
-
-        spiritSpin = (Spinner) findViewById(R.id.spirit_spinner);
-        String[] spirits = new String[]{"N/A", "Absinthe", "Amaretto", "Amaro", "Aquavit", "Arak", "Armagnac", "Baiju", "Beer",
-                "Bourbon/American Whiskey", "Brandy/Cognac", "Cachaca", "Calvados", "Canadian Whiskey", "Champagne & Sparkling Wine",
-                "Cider", "Cognar", " Eau-de-vie", "Gin", "Irish Whiskey", "Japanese Whiskey", "Liqueurs", "Mead", "Mezcal", "Non alcoholic", "Other Whiskey", "Pastis", "Pisco", "Rhum Agricole", "Rum", "Rye Whiskey", "Scotch",
-                "Sherry", "Shochu", "Soju", "Sotol", "Taiwanese Whiskey", "Tequila", "Vermouth/Aperitif Wine", "Vodka", "Whiskey", "Wine"};
-        ArrayAdapter<String> spiritAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, spirits);
-        spiritSpin.setAdapter(spiritAdapter);
-
-        glassSpin = (Spinner) findViewById(R.id.glass_spinner);
-        String[] glasses = new String[]{"N/A", "Beer Mug", "Champagne Flute", "Cocktail/Martini", "Cordial",
-                "Coupe/Coupette", "Highball/Collins", "Hurricane", "Margarita", "Mason Jar", "Moscow Mule", "Mug", "Old Fashioned/Rocks", "Pint", "Punch", "Shot", "Snifter", "Wine Glass"};
-        ArrayAdapter<String> glassAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, glasses);
-        glassSpin.setAdapter(glassAdapter);
-
-        typeSpin = (Spinner) findViewById(R.id.type_spinner);
-        String[] types = new String[]{"N/A", "Classics", "Frozen/Blended", "Hot", "Margaritas", "Martinis", "Modern Classics", "Non-Alcoholic", "Punches", "Shots", "Tiki/Tropical"};
-        ArrayAdapter<String> typeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, types);
-        typeSpin.setAdapter(typeAdapter);
-
-        strengthSpin = (Spinner) findViewById(R.id.strength_spinner);
-        String[] strengths = new String[]{"N/A", "Light", "Medium", "Strong"};
-        ArrayAdapter<String> strengthAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, strengths);
-        strengthSpin.setAdapter(strengthAdapter);
-
-        difficultySpin = (Spinner) findViewById(R.id.difficulty_spinner);
-        String[] difficulties = new String[]{"N/A", "Simple", "Medium", "Complicated"};
-        ArrayAdapter<String> difficultyAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, difficulties);
-        difficultySpin.setAdapter(difficultyAdapter);
-
-        themeSpin = (Spinner) findViewById(R.id.theme_spinner);
-        String[] themes = new String[]{"N/A", "Brunch", "chocolate", "coffee", "Dessert", "Fall", "Holiday Season", "Romantic", "Sports", "Spring", "Summer", "Travel", "Winter"};
-        ArrayAdapter<String> themeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, themes);
-        themeSpin.setAdapter(themeAdapter);
-
-        serveSpin = (Spinner) findViewById(R.id.served_spinner);
-        String[] serves = new String[]{"N/A", "Chipped Block Ice", "Crushed Ice", "Frozen", "Hot/Warm", "Large Ice Cube", "Neat/Up", "On the Rocks"};
-        ArrayAdapter<String> serveAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, serves);
-        serveSpin.setAdapter(serveAdapter);
-
-        prepSpin = (Spinner) findViewById(R.id.prep_spinner);
-        String[] preps = new String[]{"N/A", "Blended", "Cooked/Heated", "Poured", "Shaken", "Stirred"};
-        ArrayAdapter<String> prepAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, preps);
-        prepSpin.setAdapter(prepAdapter);
-
-        flavor_spin = (Spinner) findViewById(R.id.flavor_spinner);
-        String[] flavors = new String[]{"N/A", "Bitter", "Bubbly", "Creamy", "Fruity/Citrus-forward", "Herbaceous", "Salty/Savory", "Sour", "Spicy", "Spirit-forward", "Sweet"};
-        ArrayAdapter<String> flavorAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, flavors);
-        flavor_spin.setAdapter(flavorAdapter);
-
-        title = (EditText) findViewById(R.id.title);
-        about = (EditText) findViewById(R.id.about);
-        instructions = (EditText) findViewById(R.id.instructions);
-
-        auth = FirebaseAuth.getInstance();
-        authorID = auth.getCurrentUser().getUid();
-        switch (mode) {
-            case "add":
-                break;
-            case "edit":
-                oldDrinkName = prevIntent.getStringExtra("drinkName");
-                title.setText(prevIntent.getStringExtra("drinkName"), TextView.BufferType.EDITABLE);
-                about.setText(prevIntent.getStringExtra("about"), TextView.BufferType.EDITABLE);
-                instructions.setText(prevIntent.getStringExtra("instructions"), TextView.BufferType.EDITABLE);
-                String[] attributes = prevIntent.getStringArrayExtra("Attributes");
-                spiritSpin.setSelection(getIndex(spiritSpin, prevIntent.getStringExtra("spirit")));
-                glassSpin.setSelection(getIndex(glassSpin, attributes[9]));
-                typeSpin.setSelection(getIndex(typeSpin, attributes[10]));
-                strengthSpin.setSelection(getIndex(strengthSpin, attributes[6]));
-                difficultySpin.setSelection(getIndex(difficultySpin, attributes[7]));
-                themeSpin.setSelection(getIndex(themeSpin, attributes[8]));
-                serveSpin.setSelection(getIndex(serveSpin, attributes[11]));
-                prepSpin.setSelection(getIndex(prepSpin, attributes[5]));
-                flavor_spin.setSelection(getIndex(flavor_spin, prevIntent.getStringExtra("flavor")));
-        }
-    }
 
     private int getIndex(Spinner spinner, String myString) {
         int index = 0;
